b3d4a843a9c5b2a10bec87a0f47d1f77e93167c5
hungms <140624966+hungms@users.noreply.github.com>
2025-06-02 22:02:33 +0100

    enable create_seurat_obj() to read from cellranger and .h5 files

--- R/core_objects.R
+++ R/core_objects.R
@@ -1,43 +1,65 @@
 #' create_seurat_object
 #'
-#' Creates a list of Seurat objects from 10x Genomics cellranger-multi outputs.
+#' Creates a list of Seurat objects from 10x Genomics cellranger/h5 outputs.
 #' This function processes RNA, ADT (Antibody-Derived Tags), and HTO (Hashtag Oligonucleotides)
-#' modalities if present in the cellranger outputs.
+#' modalities if present in the cellranger/h5 outputs.
 #' 
-#' @param data_dirs A vector of directories containing cellranger outputs
-#' @param samples A vector of sample names for each directory
-#' @param format The format of the data to be loaded. Either "cellranger" or "h5". Defaults to "cellranger"
-#' @param hto_pattern Prefix to identify HTO tag names if HTO library is present. Defaults to NULL
-#' @param denoise_adt If TRUE, denoise ADT counts using DSB normalization. Defaults to TRUE
+#' @param dirs A vector of directories containing cellranger/h5 outputs
+#' @param run_ids A vector of run IDs for each directory
+#' @param file_format The format of the data to be loaded. Either "cellranger" or "h5". Defaults to "cellranger"
+#' @param hto_prefix Prefix to identify HTO tag names if HTO library is present. Defaults to NULL
 #' @return A list of Seurat objects, with each entry corresponding to a sample
 #' @export
-create_seurat_object <- function(data_dirs, samples, format = "cellranger", hto_pattern = NULL, denoise_adt = TRUE){    
+create_seurat_object <- function(dirs, run_ids, file_format = "cellranger", hto_prefix = NULL, merge = TRUE){    # , denoise_adt = TRUE
 
-    stopifnot(format %in% c("cellranger", "h5")) 
-    stopifnot(length(samples) == length(data_dirs))
+    # validations
+    stopifnot(file_format %in% c("cellranger", "h5")) 
+    stopifnot(length(run_ids) == length(dirs))
 
+    # initialize list
     obj.list <- list()
+    
+    # setup for loop
+    for(i in seq_along(run_ids)){
+        
+        # log progress bar
+        message(paste0("Processing ", i, " out of ", length(run_ids), " runs..."))
+        log_progress_bar(i, length(run_ids), pb = NULL)
+        
+        # read cellranger output
+        if(file_format == "cellranger"){
 
-    for(i in seq_along(samples)){
-        message(paste0("\033[1mLoading sample ", i, " for ", samples[i], "\033[0m"))
+            # validate directory and cellranger output
+            stopifnot(dir.exists(dirs[i]))
+            stopifnot(c("barcodes.tsv", "features.tsv", "matrix.mtx") %in% list.files(dirs[i]))
 
-        # read 10x output
-        message("Extracting RNA expression...")
-        if(format == "cellranger"){
-            matrix_data <- Read10X(data_dirs[i])}
-        else if(format == "h5"){
-            matrix_data <- Read10X_h5(data_dirs[i])
+            # read cellranger output
+            matrix_data <- Read10X(dirs[i])}
+
+        # read h5 output
+        else if(file_format == "h5"){
+
+            # validate directory and h5 file
+            stopifnot(file.exists(dirs[i]))
+            stopifnot(grepl(".h5$", dirs[i]))
+
+            # read h5 file
+            matrix_data <- Read10X_h5(dirs[i])
+
+            # replace underscores with dashes in feature names
             for(k in seq_along(matrix_data)){
                 rownames(matrix_data[[k]]) <- gsub("_", "-", rownames(matrix_data[[k]]))}} 
 
-        has_adt <- "Antibody Capture" %in% names(matrix_data) # check for ADT
+
+        # check if ADT assay is present
+        has_adt <- "Antibody Capture" %in% names(matrix_data)
         if(has_adt){
             rna_counts <- matrix_data$`Gene Expression`
         } else {
             rna_counts <- matrix_data
         }
-        
-        # Create Seurat object with RNA data 
+
+        # create RNA assay
         obj <- suppressWarnings(CreateSeuratObject(
             as(rna_counts, "sparseMatrix"), 
             assay = "RNA", 
@@ -45,56 +67,93 @@ create_seurat_object <- function(data_dirs, samples, format = "cellranger", hto_
             min.features = 200
         ))
 
-        # check if other modalities are present
-        message("Check if HTO and ADT modalities are present...")
+        # check if HTO assay is present
         has_hto <- FALSE
         if(has_adt){
             has_hto <- any(str_detect(rownames(matrix_data$`Antibody Capture`), hto_pattern))
             has_adt <- any(!(str_detect(rownames(matrix_data$`Antibody Capture`), hto_pattern)))}
 
-       	# if HTO is present, add it to the Seurat object
+       	# if HTO is present
         if(has_hto){
-            # extract HTO data
-            message("Extracting HTO expression...")
+
+            # extract HTO from ADT assay
             hto_data <- matrix_data$`Antibody Capture`[which(str_detect(rownames(matrix_data$`Antibody Capture`), hto_pattern)),]
+
+            # create HTO assay
             obj[["HTO"]] <- CreateAssay5Object(
                 as(hto_data[, colnames(obj[["RNA"]]$counts)], "sparseMatrix"), 
                 min.cells = 0, 
                 min.features = 0)}
 
-       	# if ADT is present, add it to the Seurat object
+       	# if ADT is present
         if(has_adt){
-            message("Extracting ADT expression...")
+            
+            # remove HTO from ADT assay
             adt_data <- matrix_data$`Antibody Capture`
             if(has_hto){
                 adt_data <- adt_data[-which(str_detect(rownames(adt_data), hto_pattern)), ]}
             
+            # create ADT assay
             obj[["ADT"]] <- CreateAssay5Object(
                 as(adt_data[, colnames(obj[["RNA"]]$counts)], "sparseMatrix"), 
                 min.cells = 0, 
                 min.features = 0)
             
-            if(denoise_adt){
-                obj <- run_dsb(
-                    obj, 
-                    dir = data_dirs[i], 
-                    denoise.counts = TRUE, 
-                    use.isotype.control = FALSE, 
-                    isotype.control.name.vec = NULL, 
-                    min.count = NULL)
-            }
+            # denoise ADT counts
+            #if(denoise_adt){
+            #    obj <- run_dsb(
+            #        obj, 
+            #        dir = dirs[i], 
+            #        denoise.counts = TRUE, 
+            #        use.isotype.control = FALSE, 
+            #        isotype.control.name.vec = NULL, 
+            #        min.count = NULL)
+            # }
         }
 
-       	# add sample id in metadata
-        message("Modifying Seurat object metadata...")
+       	# add run names to metadata
         obj <- set_assay_keys(obj)
-        obj@meta.data$samples <- rep(samples[i], ncol(obj))
+        obj@meta.data$run_ids <- rep(run_ids[i], ncol(obj))
         obj <- RenameCells(
             obj, 
-            new.names = paste0(samples[i], "_", colnames(obj)))
+            new.names = paste0(run_ids[i], "_", colnames(obj)))
         obj.list[[i]] <- obj
     }
 
-    names(obj.list) <- paste0(samples)
+    # add run names to list
+    names(obj.list) <- paste0(run_ids)
+
+    # logging
+    m1 <- paste("run_ids = ", paste0(run_ids, collapse = ", "))
+    m2 <- paste("Number of cells = ", sum(sapply(obj.list, function(x) ncol(x))))
+    m3 <- paste("Number of RNA features = ", sum(sapply(obj.list, function(x) nrow(x[["RNA"]]))))
+    m4 <- paste("Number of ADT features = ", sum(sapply(obj.list, function(x) nrow(x[["ADT"]]))))
+    m5 <- paste("Number of HTO features = ", sum(sapply(obj.list, function(x) nrow(x[["HTO"]]))))
+    m6 <- paste("Number of common features = ", length(intersect_features(obj.list)))
+    log_function(m1, m2, m3, m4, m5, m6)
+    
+    # return object
+    print(obj.list)
+
+    # merge objects
+    if(merge){
+        obj <- merge(obj.list[[1]], obj.list[c(2:length(obj.list))])}
+
+    return(obj)
+}
+
+#' split_obj
+#'
+#' Splits a Seurat object into a list of Seurat objects based on a metadata column.
+#'
+#' @param obj A Seurat object.
+#' @param split.by A metadata column name to split the object by.
+#' @return A list of Seurat objects.
+split_obj <- function(obj, split.by){
+    if(!is.null(split.by)){
+        stopifnot(split.by %in% colnames(obj@meta.data))
+        obj.list <- SplitObject(obj, split.by = split.by)}
+    else{
+        obj.list <- list(`obj` = obj)}
     return(obj.list)
 }
