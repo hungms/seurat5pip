458f37484902e09b34cb5642e47fd5acc0827e28
hungms <140624966+hungms@users.noreply.github.com>
2025-06-02 22:03:56 +0100

    change remove_features() to subset_features() for readability

--- R/core_features.R
+++ R/core_features.R
@@ -4,7 +4,7 @@
 #' both gene names and cell names.
 #'
 #' @param obj A Seurat object
-#' @return A vector of all features from the Seurat object
+#' @return A vector of all features from the Seurat object, with assay keys included in vector names
 get_all_features <- function(obj){
     all_features <- c()
     assay_names <- names(obj@assays)
@@ -15,71 +15,67 @@ get_all_features <- function(obj){
         all_features <- c(all_features, assay_features)}
     return(all_features)}
 
-#' remove_features
+#' subset_features
 #'
-#' Remove features from current assay to new assay
+#' Subset features from current assay to new assay
 #' @param obj Seurat object
 #' @param features a vector features to remove
-#' @param from.assay current assay name, defaults to "RNA"
-#' @param to.assay new assay name to store removed features
+#' @param assay.from current assay name
+#' @param assay.to new assay name to store removed features
 #' @export
-remove_features <- function(obj, features = NULL, from.assay = "RNA", to.assay){
+subset_features <- function(obj, features = NULL, assay.from, assay.to){
 
-    if(from.assay == "RNA"){
-        stopifnot("data" %in% Layers(obj, assay = "RNA"))}
+    # validate data layer
+    if(class(obj[[assay.from]]) == "Assay5" & !"data" %in% Layers(obj, assay = assay.from)){
+        stop("Please normalize your data before removing features")}
 
-    DefaultAssay(obj) <- from.assay
-    keep <- which(rownames(obj) %in% c(features))
+    # set default assay
+    DefaultAssay(obj) <- assay.from
 
-    if(length(keep) <= 1){
-        message("Detected features <= 1, stop subsetting features")
+    # find index of features to keep
+    index.to.keep <- which(rownames(obj) %in% c(features))
+    if(length(index.to.keep) <= 1){
+        warning("Number of features detected <= 1, stop subsetting")
         return(obj)}
-    else{
-        message(paste0("Detected features = ", length(keep)))}
 
-    feature.counts <- obj[[from.assay]]$counts[keep,]
-    new.counts <- obj[[from.assay]]$counts[-c(keep),]
+    # subset counts
+    assay.to.counts <- obj[[assay.from]]$counts[index.to.keep,]
+    assay.to.data <- obj[[assay.from]]$data[index.to.keep,]
+    assay.from.counts <- obj[[assay.from]]$counts[-c(index.to.keep),]
+    assay.from.data <- obj[[assay.from]]$data[-c(index.to.keep),]
 
-    if("data" %in% Layers(obj, assay = from.assay)){
-        feature.data <- obj[[from.assay]]$data[keep,]
-        new.data <- obj[[from.assay]]$data[-c(keep),]
-        obj[[to.assay]] <- CreateAssay5Object(counts = as(feature.counts, "sparseMatrix"), data = as(feature.data, "sparseMatrix"), min.features = 0, min.cells = 0)
-        obj[[from.assay]] <- CreateAssay5Object(counts = as(new.counts, "sparseMatrix"), data = as(new.data, "sparseMatrix"), min.features = 0, min.cells = 0)}
-    else{
-        obj[[to.assay]] <- CreateAssay5Object(counts = as(feature.counts, "sparseMatrix"), min.features = 0, min.cells = 0)
-        obj[[from.assay]] <- CreateAssay5Object(counts = as(new.counts, "sparseMatrix"), min.features = 0, min.cells = 0)}
+    # store assays
+    obj[[assay.to]] <- CreateAssay5Object(counts = as(assay.to.counts, "sparseMatrix"), data = as(assay.to.data, "sparseMatrix"), min.features = 0, min.cells = 0)
+    obj[[assay.from]] <- CreateAssay5Object(counts = as(assay.from.counts, "sparseMatrix"), data = as(assay.from.data, "sparseMatrix"), min.features = 0, min.cells = 0)
+    
+    # log
+    m1 <- paste0("number of features to subset = ", length(index.to.keep))
+    log_function(m1)
 
     return(obj)}
 
-#' return_features
+#' subset_vdj_features
 #'
-#' Return features from an assay to a specific assay
-#' @param obj Seurat object
-#' @param from.assay assay name from which features are taken
-#' @param to.assay assay name to which features are stored, defaults to "RNA"
-#' @export
-return_features <- function(obj, from.assay, to.assay = "RNA"){
-    counts <- rbind(obj[[from.assay]]$counts, obj[[to.assay]]$counts)
-    data <- rbind(obj[[from.assay]]$data, obj[[to.assay]]$data)
-    obj[[to.assay]] <- CreateAssay5Object(counts = counts, data = data, min.cells = 0, min.features = 0)
-    obj[[from.assay]] <- NULL
-    return(obj)}
-
-#' remove_vdj_features
-#'
-#' Remove VDJ features from an assay
+#' subset VDJ features from an assay
 #' @param obj Seurat object
 #' @param bcr if TRUE, remove BCR-VDJ features. Defaults to FALSE
 #' @param tcr if TRUE, remove TCR-VDJ features. Defaults to FALSE
 #' @param from.assay current assay name, defaults to "RNA"
 #' @export
-remove_vdj_features <- function(obj, bcr = T, tcr = T, from.assay = "RNA"){
+subset_vdj_features <- function(obj, assay, bcr = T, tcr = T){
+
+    # if subset BCR features
     if(bcr){
-        bcr.features <- rownames(obj[[from.assay]])[which(str_detect(rownames(obj[[from.assay]]), bcr.string))]
-        obj <- remove_features(obj, features = bcr.features, from.assay = from.assay, to.assay = "BCR")}
+        message("LOG : subset_vdj_features for BCR")
+        bcr.features <- rownames(obj[[assay]])[which(str_detect(rownames(obj[[assay]]), bcr.string))]
+        obj <- subset_features(obj, features = bcr.features, assay.from = assay, assay.to = "BCR")}
+
+    # if subset TCR features
     if(tcr){
-        tcr.features <- rownames(obj[[from.assay]])[which(str_detect(rownames(obj[[from.assay]]), tcr.string))]
-        obj <- remove_features(obj, features = tcr.features, from.assay = from.assay, to.assay = "TCR")}
+        message("LOG : subset_vdj_features for TCR")
+        tcr.features <- rownames(obj[[assay]])[which(str_detect(rownames(obj[[assay]]), tcr.string))]
+        obj <- subset_features(obj, features = tcr.features, assay.from = assay, assay.to = "TCR")}
+    
     return(obj)}
 
 #' intersect_features
@@ -89,10 +85,18 @@ remove_vdj_features <- function(obj, bcr = T, tcr = T, from.assay = "RNA"){
 #' @param assay assay name
 #' @param min.cells minimum number of cells expressing the gene
 #' @export
-intersect_features <- function(obj.list, assay = "RNA", min.cells = 3){
+intersect_features <- function(obj, split.by, assay){
+
+    # split object
+    obj.list <- split_obj(obj, split.by)
+
+    # initialize list
     intersect.list <- list()
+
+    # get features from each object
     for(i in seq_along(obj.list)){
-        keep <- rowSums(obj.list[[i]][[assay]]$counts >= 1) >= min.cells
-        intersect.list[[i]] <- rownames(obj.list[[i]][[assay]]$counts)[keep]}
+        intersect.list[[i]] <- rownames(obj.list[[i]][[assay]]$counts)}
+
+    # intersect features across all objects
     features <- Reduce(intersect, intersect.list)
     return(features)}
