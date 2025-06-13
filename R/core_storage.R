3d4e5396af37266d758e5a8b22082b8ade89e11e
hungms <140624966+hungms@users.noreply.github.com>
2025-06-13 17:17:34 +0100

    add write_png()

--- R/core_storage.R
+++ R/core_storage.R
@@ -17,3 +17,30 @@ write_matrix <- function(obj, assay, dir){
         write(x = colnames(mat), file = paste0(dir, "/", l, "/barcodes.tsv")) 
     }   
 }
+
+#' Write a png file
+#'
+#' This function writes a png file to a directory.
+#'
+#' @param plot The plot to write
+#' @param output_dir The directory to write the plot to
+#' @param filename The name of the plot
+#' @param ... Additional arguments to pass to the png function
+#' @return None. Creates a png file in the specified directory
+write_png <- function(plot, output_dir, filename, ...){
+
+    stopifnot(all(is.character(c(output_dir, filename))))
+    stopifnot(str_detect(filename, "\\.png$"))
+    stopifnot(dir.exists(output_dir))
+
+    if(!is.null(output_dir)){
+        dir.create(output_dir, recursive = T)}
+
+    Cairo::CairoPNG(file.path(output_dir, filename), res = 300, ...)
+    print(plot)
+    dev.off()
+}
+
+
+
+
